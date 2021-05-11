b03a9eda6a155d64facd5ee9d6ed6254e7ccebbf
Florian Theron <florian.theron@epitech.eu>
2021-05-11 14:23:33 +0200

    Add User entity

---
+++ source/Clipify.Domain/Common/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Clipify.Domain.Common
+{
+    public abstract class Entity
+    {
+        public string Id { get; set; } = String.Empty;
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Updated { get; set; }
+
+        public string CreatedBy { get; set; } = String.Empty;
+
+        public string UpdatedBy { get; set; } = String.Empty;
+    }
+}
---
+++ source/Clipify.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Clipify.Domain.Common;
+
+namespace Clipify.Domain.Entities
+{
+    public class User : Entity
+    {
+        public string Username { get; set; } = String.Empty;
+
+        public string Password { get; set; } = String.Empty;
+    }
+}
