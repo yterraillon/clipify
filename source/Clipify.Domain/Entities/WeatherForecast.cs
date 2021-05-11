49dbeb5400634c828a9914607aa37e85708142af
Florian Theron <florian.theron@epitech.eu>
2021-05-11 11:36:47 +0200

    Init string with string.Empty

--- source/Clipify.Domain/Entities/WeatherForecast.cs
+++ source/Clipify.Domain/Entities/WeatherForecast.cs
@@ -4,17 +4,12 @@ namespace Clipify.Domain.Entities
 {
     public class WeatherForecast
     {
-        public WeatherForecast()
-        {
-            Summary = string.Empty;
-        }
-
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string Summary { get; set; } = string.Empty;
     }
 }
