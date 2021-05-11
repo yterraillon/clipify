5650f8e9c7fb1015dc9874aa7850dc9611434f05
Florian Theron <florian.theron@epitech.eu>
2021-05-11 14:09:06 +0200

    Update WeatherForecast.cs

--- source/Clipify.Application/Common/Models/WeatherForecast.cs
+++ source/Clipify.Application/Common/Models/WeatherForecast.cs
@@ -1,18 +1,9 @@
 ﻿using System;
 
-<<<<<<< HEAD:source/Clipify.Domain/Entities/WeatherForecast.cs
-namespace Clipify.Domain.Entities
-=======
 namespace Clipify.Application.Common.Models
->>>>>>> main:source/Clipify.Application/Common/Models/WeatherForecast.cs
 {
     public class WeatherForecast
     {
-        public WeatherForecast()
-        {
-            Summary = string.Empty;
-        }
-
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
