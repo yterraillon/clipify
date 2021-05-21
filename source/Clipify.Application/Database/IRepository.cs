1e36abbfd31013d4606fed68d8967c320bf3c567
Florian Theron <florian.theron@epitech.eu>
2021-05-21 16:00:11 +0200

    Move IDbContext to Database folder. Add IRepository interface.

---
+++ source/Clipify.Application/Database/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Clipify.Domain.Common;
+
+namespace Clipify.Application.Database
+{
+    public interface IRepository<out TResult, in TEntity, in TId> where TResult : class
+    {
+        TResult Get(TId id);
+
+        IEnumerable<TResult> GetAll();
+
+        void Add(TEntity entity);
+
+        void Remove(TId id);
+    }
+}
