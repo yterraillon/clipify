07b5a3e17c4eac24bde2bc651f9eaa482c511fce
Florian Theron <florian.theron@epitech.eu>
2021-07-05 12:09:19 +0200

    Update HttpClientExtensions.cs

--- source/Clipify.Infrastructure/Extensions/HttpClientExtensions.cs
+++ source/Clipify.Infrastructure/Extensions/HttpClientExtensions.cs
@@ -12,31 +12,22 @@ namespace Clipify.Infrastructure.Extensions
     {
         public static async Task<T> PostRequestAsync<T>(this HttpClient client, Uri requestUri, HttpMethod method, IDictionary<string, string>? parameters = null, CancellationToken cancellationToken = new())
         {
-            try
-            {
-                var response = await client
-                    .SendAsync(new HttpRequestMessage(method, requestUri)
-                    {
-                        Content = parameters != null ? new FormUrlEncodedContent(parameters!) : null
-                    }, cancellationToken);
-
-                response.EnsureSuccessStatusCode();
-
-                var content = await response.Content
-                    .ReadAsStringAsync(cancellationToken);
-
-                return JsonConvert.DeserializeObject<T>(content, new JsonSerializerSettings
+            var response = await client
+                .SendAsync(new HttpRequestMessage(method, requestUri)
                 {
-                    DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate,
-                    NullValueHandling = NullValueHandling.Ignore,
-                }) ?? throw new JsonSerializationException("Failed to deserialize response content.");
-            }
-            catch (Exception e)
+                    Content = parameters != null ? new FormUrlEncodedContent(parameters!) : null
+                }, cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content
+                .ReadAsStringAsync(cancellationToken);
+
+            return JsonConvert.DeserializeObject<T>(content, new JsonSerializerSettings
             {
-                // TODO: Logger?
-                Console.WriteLine(e);
-                throw;
-            }
+                DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate,
+                NullValueHandling = NullValueHandling.Ignore
+            }) ?? throw new JsonSerializationException("Failed to deserialize response content.");
         }
 
         public static async Task<T> GetWithQueryParametersAsync<T>(this HttpClient client, string requestUri,
