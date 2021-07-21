533a65d1e400721dfbb0d0c5f2232eaa6bb9e5a8
Florian Theron <florian.theron@epitech.eu>
2021-07-21 09:56:49 +0200

    Get and store new tracks

--- source/Clipify.Application/Playlists/Commands/SyncPlaylists/SyncPlaylist.cs
+++ source/Clipify.Application/Playlists/Commands/SyncPlaylists/SyncPlaylist.cs
@@ -16,33 +16,53 @@ namespace Clipify.Application.Playlists.Commands.SyncPlaylists
 
         public class Handler : BaseUserHandler, IRequestHandler<Command, bool>
         {
-            private readonly IRepository<ForkedPlaylist, string> _forkedPlaylistRepository;
+            private readonly IRepository<Playlist, string> _playlistRepository;
+
+            private readonly IRepository<Track, string> _trackRepository;
 
             private readonly IPlaylistClient _playlistClient;
 
-            public Handler(IRepository<ForkedPlaylist, string> forkedPlaylistRepository, IPlaylistClient playlistClient,
-                ICurrentUserService currentUserService) : base(currentUserService)
+            private readonly IPlaylistService _playlistService;
+            
+            public Handler(IRepository<Playlist, string> playlistRepository, IPlaylistClient playlistClient,
+                ICurrentUserService currentUserService, IRepository<Track, string> trackRepository,
+                IPlaylistService playlistService) : base(currentUserService)
             {
-                _forkedPlaylistRepository = forkedPlaylistRepository;
+                _playlistRepository = playlistRepository;
                 _playlistClient = playlistClient;
+                _trackRepository = trackRepository;
+                _playlistService = playlistService;
             }
 
             /// <inheritdoc />
             public async Task<bool> Handle(Command request, CancellationToken cancellationToken)
             {
-                var localPlaylist = _forkedPlaylistRepository.Get(request.PlaylistId);
-                var playlist = await _playlistClient.GetPlaylistAsync(CurrentUser.AccessToken,
-                    CurrentUser.UserId, localPlaylist.OriginalPlaylistId, cancellationToken);
+                var localPlaylist = _playlistRepository.Get(request.PlaylistId);
 
+                if (localPlaylist.Equals(Playlist.Empty))
+                    return false;
+                
+                var playlist = await _playlistClient.GetPlaylistWithTracksAsync(CurrentUser.AccessToken,
+                    localPlaylist.PlaylistId, cancellationToken);
+
+                localPlaylist.RegisterLastCheck();
+                
                 if (!localPlaylist.IsOutdated(playlist.SnapshotId))
                     return true;
 
+                localPlaylist.Update(playlist.Name, playlist.SnapshotId);
+
+                foreach (var track in playlist.Tracks)
+                {
+                    if (_trackRepository.Any(t => t.TrackId == track.Id && t.PlaylistId == playlist.Id))
+                        continue;
+                    
+                    _trackRepository.Add(Track.Create(track.Id, playlist.Id, track.Uri));
+                }
+                
                 // TODO: Get Tracks.
                 // TODO: Update ForkedPlaylist.
-                // TODO: Update original Playlist?
                 
-                localPlaylist.RegisterLastSync(playlist.SnapshotId);
-
                 return true;
             }
         }
