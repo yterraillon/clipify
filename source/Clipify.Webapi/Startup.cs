9e44552d3e8b6ae94f83c0726f3c823ec7d90cee
Florian Theron <florian.theron@epitech.eu>
2021-08-03 11:07:24 +0200

    Update DI

--- source/Clipify.Webapi/Startup.cs
+++ source/Clipify.Webapi/Startup.cs
@@ -1,4 +1,5 @@
 using Clipify.Application;
+using Clipify.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,7 @@ namespace Clipify.Webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
             services.AddApplication();
             services.AddControllers();
             services.AddSwaggerGen(c =>
